a4531592487a65cdfbcd91cc5b9c80e1ec2d271e
Christoph Ostrau <costrau@techfak.uni-bielefeld.de>
2019-12-09 17:33:51 +0100

    Update to Python3

--- cmake/cypress_external.cmake
+++ cmake/cypress_external.cmake
@@ -15,8 +15,8 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-find_package(PythonLibs 2.7 REQUIRED )
-find_package(PythonInterp 2.7 REQUIRED)
+find_package(PythonLibs 3 REQUIRED )
+find_package(PythonInterp 3 REQUIRED)
 
 include(ExternalProject)
 
@@ -26,8 +26,8 @@ set(UPDATE_CYPRESS FALSE CACHE BOOL "True for update cypress every time")
 if(UPDATE_CYPRESS)
     ExternalProject_Add(cypress_ext
         GIT_REPOSITORY        "https://github.com/hbp-unibi/cypress/"
-        GIT_TAG               genn
-        CMAKE_ARGS            -DSTATIC_LINKING=${STATIC_LINKING} -DCMAKE_INSTALL_PREFIX:path=<INSTALL_DIR> -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} -DBUILD_TEST_EXAMPLES=False
+        GIT_TAG               master
+        CMAKE_ARGS            -DSTATIC_LINKING=${STATIC_LINKING} -DCMAKE_INSTALL_PREFIX:path=<INSTALL_DIR> -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} -DBUILD_TEST_EXAMPLES=False  -DPYTHON_VERSION_MAJOR=${PYTHON_VERSION_MAJOR}
         INSTALL_COMMAND 	  ""
         UPDATE_COMMAND git pull
         EXCLUDE_FROM_ALL      TRUE
@@ -36,8 +36,8 @@ if(UPDATE_CYPRESS)
 else()
     ExternalProject_Add(cypress_ext
         GIT_REPOSITORY        "https://github.com/hbp-unibi/cypress/"
-        GIT_TAG               genn
-        CMAKE_ARGS            -DSTATIC_LINKING=${STATIC_LINKING} -DCMAKE_INSTALL_PREFIX:path=<INSTALL_DIR> -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} -DBUILD_TEST_EXAMPLES=False
+        GIT_TAG               master
+        CMAKE_ARGS            -DSTATIC_LINKING=${STATIC_LINKING} -DCMAKE_INSTALL_PREFIX:path=<INSTALL_DIR> -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} -DBUILD_TEST_EXAMPLES=False -DPYTHON_VERSION_MAJOR=${PYTHON_VERSION_MAJOR}
         INSTALL_COMMAND 	  ""
         UPDATE_COMMAND ""
         EXCLUDE_FROM_ALL      TRUE
